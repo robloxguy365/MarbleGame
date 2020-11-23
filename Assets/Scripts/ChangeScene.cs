5064a9ab874d09123e7857f865a037c11033c8bf
robloxguy365 <56357107+robloxguy365@users.noreply.github.com>
2020-11-22 21:40:05 -0500

    Added ability to change scenes and quit game
    Options Button and Back Button are now functional and the quit button quits the game. Scripts for changing scenes ans quitting game added.

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void LoadNextLevel(int sceneNum)
+    {
+        SceneManager.LoadScene(sceneNum);
+    }
+    public void LoadNextLevel(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
